0939911e3eec0c831ff448eefd61d1b7b3cace65
SD2400 <19331074116@163.com>
2026-09-04 20:53:44 +0800

    运算符

--- 04.运算符.py
+++ 04.运算符.py
@@ -98,5 +98,5 @@
 # print(f"{num}是否在10-20之间:",num >= 10 and num <=20)
 
 #键盘输入一个整数,判断这个整数是否不在10-20之间
-# num = int(input("请输入一个整数:"))
-# print(f"{num}是否不在10-20之间:",num < 10 or num > 20)
+num = int(input("请输入一个整数:"))
+print(f"{num}是否不在10-20之间:",num < 10 or num > 20)
